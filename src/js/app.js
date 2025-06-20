0eee882b1749068c0dfae8f490224820f6f65c84
NicolleMendoza30 <83618165+NicolleMendoza30@users.noreply.github.com>
2025-06-20 23:48:02 +0000

    Profile-NicolleMendoza

--- src/js/app.js
+++ src/js/app.js
@@ -28,15 +28,18 @@ function render(variables = {}) {
   // if includeCover==false then we reset the cover code without the <img> tag to make the cover transparent.
   let cover = `<div class="cover"><img src="${variables.background}" /></div>`;
   if (variables.includeCover == false) cover = "<div class='cover'></div>";
-
   // reset the website body with the new html output
   document.querySelector("#widget_content").innerHTML = `<div class="widget">
             ${cover}
           <img src="${variables.avatarURL}" class="photo" />
-          <h1>Lucy Boilett</h1>
-          <h2>Web Developer</h2>
-          <h3>Miami, USA</h3>
-          <ul class="position-right">
+          <h1>${variables.name === null ? "" : variables.name} ${
+    variables.lastName === null ? "" : variables.lastName
+  }</h1>
+          <h2>${variables.role === null ? "" : variables.role}</h2>
+          <h3>${variables.city === null ? "" : variables.city}${
+    variables.country === null ? "" : variables.country
+  }</h3>
+          <ul class=${variables.socialMediaPosition}>
             <li><a href="https://twitter.com/4geeksacademy"><i class="fab fa-twitter"></i></a></li>
             <li><a href="https://github.com/4geeksacademy"><i class="fab fa-github"></i></a></li>
             <li><a href="https://linkedin.com/school/4geeksacademy"><i class="fab fa-linkedin"></i></a></li>
@@ -45,7 +48,6 @@ function render(variables = {}) {
         </div>
     `;
 }
-
 /**
  * Don't change any of the lines below, here is where we do the logic for the dropdowns
  */
@@ -71,7 +73,6 @@ window.onload = function() {
     city: null
   };
   render(window.variables); // render the card for the first time
-
   document.querySelectorAll(".picker").forEach(function(elm) {
     elm.addEventListener("change", function(e) {
       // <- add a listener to every input
